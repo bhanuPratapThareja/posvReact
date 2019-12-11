0bef15a0e56599853dec3b438a89ca35dd849727
Bhanu_Thareja <bhanu.thareja@qualtechedge.com>
2019-12-11 15:34:08 +0530

    ques

--- src/components/Questionnair/Questionnair.js
+++ src/components/Questionnair/Questionnair.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
+import Loader from './../Loader/Loader';
 import { getQuestions } from './../../actions/questionnair/actions_questionnair';
 
 class Questionnair extends Component {
 
-    componentDidMount() {
-        this.props.getQuestions('PRODUCT')
+    constructor(){
+        this.state = {
+            proceeding: false,
+            questions: [],
+            parentQuestions: [],
+            childQuestions: [],
+            allFieldsMandatoryError: false
+        }
     }
+
+    componentDidMount() {
+        const qstCatName = this.props.location.pathname.split('/')[2].toUpperCase();
+        this.setState({ qstCatName }, () => {
+            this.getQuestions();
+        })
+    }
+
+    getQuestions = () => {
+        this.setState({ proceeding: true })
+        this.props.getQuestions()
+    }
+
     render() {
-        return (
-            <>
-                <p>Questionnair</p>
-            </>
-        )
+        if (this.state.proceeding) {
+            return <Loader />
+        }
     }
 }
 
