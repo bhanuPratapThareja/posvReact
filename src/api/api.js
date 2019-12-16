91192d7dc907f0d039a773acd2e0a4ce2c383d50
Bhanu_Thareja <bhanu.thareja@qualtechedge.com>
2019-12-16 15:24:21 +0530

    mli dev env added

--- src/api/api.js
+++ src/api/api.js
@@ -1,57 +1,65 @@
-const uatMode = true;
+export const mode = 'mliDev'; // qualTechDev // mliDev
 
 const api = {
-    baseUrl: 'https://n913i5xkoi.execute-api.ap-south-1.amazonaws.com/',
+    qualTechDevbaseUrl: 'https://n913i5xkoi.execute-api.ap-south-1.amazonaws.com/',
+    mliDevBaseUrl: 'https://djedtvaxn2.execute-api.ap-south-1.amazonaws.com/posvdev/',
     routes: {
         verifyUser: {
-            uatUrl: 'Stage/verify-customer-link',
-            prodUrl: 'prod/verify-customer-link',
+            qualTechDevUrl: 'Stage/verify-customer-link',
+            mliDevUrl: 'validate-customer-link-dev',
             body: { "request": { "header": { "appId": "mpro", "correlationId": "121324324" }, "payload": { "posvRefNumber": "343030303035323234397c7c6d70726f7c7c616263" } } }
         },
         verifyCustomerImage: {
-            uatUrl: 'Stage/verify-customer-image',
-            prodUrl: 'prod/verify-customer-image',
+            qualTechDevUrl: 'Stage/verify-customer-image',
+            mliDevUrl: 'validate-customer-image-dev',
             body: { "request": { "header": { "appId": "mpro", "correlationId": "25478965874" }, "payload": { "posvRefNumber": "", imageFile: "", authToken: "", fileExtension: "" } } }
         },
         getotp: {
-            uatUrl: 'Stage/getotp',
-            prodUrl: 'prod/getotp',
+            qualTechDevUrl: 'Stage/getotp',
+            mliDevUrl: 'get-otp-dev',
             body: { "request": { "header": { "correlationId": "25478965874", "appId": "ABC" }, "payload": { "posvRefNumber": "", "authToken": "", "onCallOTP":"No" } } },
         },
         validateOtp: {
-            uatUrl: 'Stage/validateotp',
-            prodUrl: 'prod/validateotp',
+            qualTechDevUrl: 'Stage/validateotp',
+            mliDevUrl: 'validate-otp-dev',
             body: { "request": { "header": { "correlationId": "25478965874", "appId": "ABC" }, "payload": { "posvRefNumber": "", "otp": "", "authToken": "" } } }
         },
         declaration: {
-            uatUrl: 'Stage/customer-disclaimer',
-            prodUrl: 'prod/customer-disclaimer',
+            qualTechDevUrl: 'Stage/customer-disclaimer',
+            mliDevUrl: 'customer-disclaimer-dev',
             body: { "request": { "header": { "correlationId": "25478965874", "appId": "ABC" }, "payload": { "posvRefNumber": "", "customerDisclaimer": "", "authToken": "" } } }
         },
         pdf: {
-            uatUrl: 'Stage/get-pdf-transcript',
-            prodUrl: 'prod/get-pdf-transcript',
+            qualTechDevUrl: 'Stage/get-pdf-transcript',
+            mliDevUrl: 'get-pdf-transcript-dev',
             body: { "request": { "header": { "correlationId": "25478965874", "appId": "mpro" }, "payload": { "posvRefNumber": "", "authToken": "" } } }
         },
         getQuestions: {
-            uatUrl: 'Stage/get-questions',
-            prodUrl: 'prod/get-questions',
+            qualTechDevUrl: 'Stage/get-questions',
+            mliDevUrl: 'get-questions-dev',
             body: { "request": { "header": { "appId": "mpro", "correlationId": "25478965874" }, "payload": { "qstCatName": "", "qstSubCatName": null, "posvRefNumber": "", "qstCatNameNext": null, "qstCatNamePrevious": null, "LANG": "ENG", "authToken": "", "planCode": "" } } }
         },
         saveCustomerResponse: {
-            uatUrl: 'Stage/save-customer-response',
-            prodUrl: 'prod/save-customer-response',
+            qualTechDevUrl: 'Stage/save-customer-response',
+            mliDevUrl: 'save-customer-response-dev',
             body: { "request": { "header": { "appId": "mpro", "correlationId": "25478965874" }, "payload": { "customerResponse": { "qst": [] }, "qstCatName": "", "qstSubCatName": null, "posvRefNumber": "4000052249", "lang": "EN", "planCode": "EFGEP8", "qstCatNameNext": null, "qstSubCatNameNext": null, "qstCatNamePrevious": null, "qstSubCatNamePrevious": null, "authToken": "wtewe834jwe" } } }
         }
     }
 }
 
 const getUrl = route => {
-    const baseUrl = `${api.baseUrl}`;
-    if (uatMode) {
-        return `${baseUrl}${api['routes'][route]['uatUrl']}`;
+    let baseUrl;
+    let url;
+    if(mode === 'qualTechDev'){
+        baseUrl = `${api.qualTechDevbaseUrl}`;
+        url =  `${baseUrl}${api['routes'][route]['qualTechDevUrl']}`;
     }
-    return `${baseUrl}${api['routes'][route]['prodUrl']}`;
+    if(mode === 'mliDev'){
+        baseUrl = `${api.mliDevBaseUrl}`;
+        url =  `${baseUrl}${api['routes'][route]['mliDevUrl']}`;
+    }
+    console.log('url: ', url)
+    return url;
 }
 
 const getBody = route => {
--- src/api/headers.js
+++ src/api/headers.js
@@ -1,3 +1,19 @@
-export const headers = { 
-    'x-api-key': 'U8eL0A3syl3wPN0U1tMuN7OJH66cXw0llUlC4deg' 
+import { mode } from './api';
+
+const getHeader = () => {
+    console.log('mode:: ', mode)
+    let header;
+    switch(mode){
+        case 'qualTechDev':
+            header = 'U8eL0A3syl3wPN0U1tMuN7OJH66cXw0llUlC4deg';
+            break;
+        case 'mliDev':
+            header = 'niu7mbEQoX1rGcOOEEOVV4x289vK8MT89D5Dd0nR';
+            break;
+    }
+    return header
+}
+
+export const headers = {
+    'x-api-key': getHeader() 
 }
