16528d50498a11b3446538ea0a1265cad51cbf83
Bhanu_Thareja <bhanu.thareja@qualtechedge.com>
2019-12-20 17:31:12 +0530

    bug fix

--- src/utils/dateYear.js
+++ src/utils/dateYear.js
@@ -1,43 +1,30 @@
 export const isDateYearValid = dateYear => {
     const month = dateYear.substr(0,2);
     const year = dateYear.substr(3);
+    let error = '';
     if(year.indexOf('_') !== -1){
-        return {
-            error: 'Invalid date'
-        }
-    }
-    if(dateYear.length < 7){
-        return {
-            error: 'Invalid date'
-        }
+        error ='Invalid date';
     }
     if(month === '00'){
-        return {
-            error: 'Month cannot be 00'
-        }
+        error = 'Month cannot be 00'
+    
     }
     if(month > '12'){
-        return {
-            error: 'Month cannot be greater than 12'
-        }
+        error = 'Month cannot be greater than 12'
+    
     }
     const d = new Date;
     if(year < 1900){
-        return {
-            error: 'Year cannot be less than 1900'
-        }
+        error = 'Year cannot be less than 1900'
+    
     }
     if(year > d.getFullYear()){
-        return {
-            error: 'Year cannot be greater than current year'
-        }
+        error = 'Year cannot be greater than current year'
+    
     }
     if(year == d.getFullYear() && month > d.getMonth() + 1){
-        return {
-            error: 'Month cannot be greater than current month'
-        }
+        error = 'Month cannot be greater than current month'
+    
     }
-    return {
-        error: ''
-    };
+    return { error };
 }
