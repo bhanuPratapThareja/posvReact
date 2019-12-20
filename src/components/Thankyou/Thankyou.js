e0f57d8cb4888b249091c79b346b67a784c99936
Bhanu_Thareja <bhanu.thareja@qualtechedge.com>
2019-12-21 00:38:21 +0530

    thankyou page amimation added

--- src/components/Thankyou/Thankyou.js
+++ src/components/Thankyou/Thankyou.js
@@ -16,8 +16,9 @@ const Thankyou = (props) => {
         <Grid container justify="center" alignItems="center" className="cstm-wrap">
             <Grid item>
                 <div className="thank_you">
-                    <CheckCircleOutlineIcon style={{ fontSize: '50px', fill: '#00236e' }} />
-                    <p>Thank you for your response</p>
+                    {/* <CheckCircleOutlineIcon style={{ fontSize: '50px', fill: '#00236e' }} /> */}
+                    <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
+                    <p style={{ marginTop: '50px' }}>Thank you for your response</p>
                 </div>
             </Grid>
         </Grid>
